73f54938b724e82793ee114570e5645f3b07c2bc
Ahmed Emad <emada2826@gmail.com>
2024-11-30 09:37:43 +0200

    First commit

---
+++ resources/views/home.blade.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>This is the home page</title>
+</head>
+<body>
+
+    <div style="color: brown"> <H1>Home Page</H1></div>
+</body>
+</html>
