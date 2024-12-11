1c4c64075cf64dbcc148e52fa7cb5cda7e05b8f7
AhmedEmad101 <95496311+AhmedEmad101@users.noreply.github.com>
2024-12-11 15:35:41 +0200

    I added Admin middleware

--- routes/api.php
+++ routes/api.php
@@ -7,7 +7,7 @@ use App\Http\Controllers\TaskController;
 use App\Http\Controllers\UserController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
-
+use App\Http\Middleware\Admin;
 Route::get('/user', function (Request $request) {
     return $request->user();
 })->middleware('auth:sanctum');
@@ -16,7 +16,8 @@ Route::get('/user', function (Request $request) {
 route::post('login',[AuthController::class,'Login']);
 route::post('logout',[AuthController::class,'logout']);
 route::get('user/{id}',action: [UserController::class,'UserInfo']);
-route::get('test/{id}',action: [UserController::class,'test'])->middleware('auth:sanctum');;
+route::get('test/{id}',action: [UserController::class,'test'])->middleware('auth:sanctum',Admin::class);
+route::get('test',action: [UserController::class,'test'])->middleware('auth:sanctum' , Admin::class);
 ////////////////////////////////////////////////////////
 Route::get('alltasks', [TaskController::class, 'index']);
 Route::get('allusers', [UserController::class, 'index']);
